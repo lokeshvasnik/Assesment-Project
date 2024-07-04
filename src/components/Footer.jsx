6e55875a400d8a4e2022c3432d675734373ec994
lokeshvasnik <lokeshvasnik2003@gmail.com>
2024-07-04 22:13:58 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,124 @@
+import { ArrowUp, MoveUpRight } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    function topBtnHandler() {
+        document
+            .getElementById("scrollToTopBtn")
+            .addEventListener("click", () => {
+                window.scrollTo({ top: 0, behavior: "smooth" });
+            });
+    }
+    return (
+        <footer className="mt-10 py-5">
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-3 px-10 items-center justify-center">
+                <div className="col-span-1 flex justify-center md:justify-start">
+                    <ul className="flex bg-[#10a958] justify-center items-center flex-col md:text-4xl space-y-2 font-semibold   p-4 rounded-full">
+                        <li>THE</li>
+                        <li>FIGMA</li>
+                        <li>STORE</li>
+                    </ul>
+                </div>
+                <div className="col-span-1 flex justify-center md:justify-center text-center md:text-left">
+                    <h1 className="text-xl md:text-3xl font-bold">
+                        OBJECTS <br /> THAT INSPIRE.
+                    </h1>
+                </div>
+                <div className="col-span-1 flex justify-center md:justify-end">
+                    <ul className="text-sm md:text-lg space-y-2 font-semibold text-center md:text-right">
+                        <li>
+                            <a href="#faq" className="hover:underline">
+                                FAQ
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#contact" className="hover:underline">
+                                Contact Us
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#privacy" className="hover:underline">
+                                Privacy Policy
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#terms" className="hover:underline">
+                                Terms of Service
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-span-1 md:col-span-3 flex flex-col md:flex-row justify-center md:justify-between items-center mt-5 md:mt-10">
+                    <div className="flex space-x-4 md:space-x-10 text-sm md:text-3xl">
+                        <Link
+                            target="_blank"
+                            to="https://www.figma.com"
+                            className="hover:underline cursor-pointer list-none flex justify-center items-center"
+                        >
+                            FIGMA{" "}
+                            <span>
+                                <MoveUpRight
+                                    strokeWidth={3.5}
+                                    className="mx-2"
+                                />
+                            </span>
+                        </Link>
+                        <li className="hover:underline cursor-pointer list-none">
+                            TWITTER
+                        </li>
+                        <li className="hover:underline cursor-pointer list-none">
+                            INSTAGRAM
+                        </li>
+                        <li className="hover:underline cursor-pointer list-none">
+                            YOUTUBE
+                        </li>
+                    </div>
+                    <button
+                        id="scrollToTopBtn"
+                        className="p-3 bg-black rounded-full mt-3"
+                        onClick={topBtnHandler}
+                    >
+                        <ArrowUp color="#fff" />
+                    </button>
+                </div>
+            </div>
+            <div className="marquee">
+                <div className="marquee__slider">
+                    <span>COLLECTION</span>
+                    <span>OF</span>
+                    <span>OBJECTS</span>
+                    <span>FOR</span>
+                    <span>OUR</span>
+                    <span>TIME</span>
+                    <span>ON</span>
+                    <span>FIGMA&apos;S</span>
+                    <span>MARVELOUSLY</span>
+                </div>
+                <div className="marquee__slider">
+                    <span>COLLECTION</span>
+                    <span>OF</span>
+                    <span>OBJECTS</span>
+                    <span>FOR</span>
+                    <span>OUR</span>
+                    <span>TIME</span>
+                    <span>ON</span>
+                    <span>FIGMA&apos;S</span>
+                    <span>MARVELOUSLY</span>
+                </div>
+                <div className="marquee__slider">
+                    <span>COLLECTION</span>
+                    <span>OF</span>
+                    <span>OBJECTS</span>
+                    <span>FOR</span>
+                    <span>OUR</span>
+                    <span>TIME</span>
+                    <span>ON</span>
+                    <span>FIGMA&apos;S</span>
+                    <span>MARVELOUSLY</span>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,124 @@
+import { Search, UserRound } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    function inputHandlerFn() {
+        document.querySelector(".inputSearch").classList.toggle("show");
+    }
+
+    function hamBurgerHandler() {
+        document.getElementById("mobileMenu").classList.toggle("show");
+    }
+
+    return (
+        <section>
+            {/* <!-- Input Field --> */}
+            <div className="inputSearch">
+                <input
+                    type="text"
+                    className="text-4xl w-full border-black px-10 py-5 border-b-4 outline-none"
+                    placeholder="Enter"
+                />
+            </div>
+            {/* <!-- Navbar --> */}
+            <nav className="py-5 px-10">
+                <div className="topnav_menu flex justify-between items-center">
+                    <div className="flex items-center hidden md:flex">
+                        <button className="navBtn py-1 px-5 border-2 border-black rounded-full font-bold text-sm mx-1">
+                            SHOP
+                        </button>
+                        <button className="navBtn py-1 px-5 border-2 border-black rounded-full font-bold mx-1 text-sm">
+                            ABOUT
+                        </button>
+                        <Search
+                            className="cursor-pointer mx-2"
+                            onClick={inputHandlerFn}
+                        />
+                    </div>
+                    <div className="flex space-x-2 navbarMob  items-center">
+                        <Search
+                            className="cursor-pointer mx-2 md:hidden"
+                            onClick={inputHandlerFn}
+                        />
+
+                        <Link to="/">
+                            <h1 className="md:text-2xl font-bold">
+                                THE FIGMA STORE
+                            </h1>
+                        </Link>
+                    </div>
+                    <div className="flex items-center hidden md:flex">
+                        <UserRound className="mx-2" />
+                        <select
+                            name="option"
+                            id="optim"
+                            className="navBtn py-1 px-5 border-2 border-black rounded-full font-bold mx-1 text-sm"
+                        >
+                            <option value="UNITED STATES">UNITED STATES</option>
+                            <option value="UNITED KINGDOM">
+                                UNITED KINGDOM
+                            </option>
+                            <option value="JAPAN">JAPAN</option>
+                            <option value="CANADA">CANADA</option>
+                            <option value="EUROPEAN UNION">
+                                EUROPEAN UNION
+                            </option>
+                        </select>
+                        <button className="navBtn py-1 px-5 border-2 border-black rounded-full font-bold mx-1 text-sm">
+                            CART 0
+                        </button>
+                    </div>
+                    <div className="md:hidden">
+                        <button
+                            id="hamburger"
+                            className="focus:outline-none"
+                            onClick={hamBurgerHandler}
+                        >
+                            <svg
+                                className="w-6 h-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16m-7 6h7"
+                                ></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div
+                    id="mobileMenu"
+                    className="hidden md:hidden flex-col items-center mt-4"
+                >
+                    <button className="navBtn py-1 px-5 border-2 border-black rounded-full font-bold text-sm mx-1 my-1">
+                        SHOP
+                    </button>
+                    <button className="navBtn py-1 px-5 border-2 border-black rounded-full font-bold text-sm mx-1 my-1">
+                        ABOUT
+                    </button>
+                    <button className="navBtn py-1 px-5 border-2 border-black rounded-full font-bold text-sm mx-1 my-1">
+                        CART 0
+                    </button>
+                    <select
+                        name="option"
+                        id="optim"
+                        className="navBtn py-1 px-5 border-2 border-black rounded-full font-bold mx-1 text-sm"
+                    >
+                        <option value="UNITED STATES">UNITED STATES</option>
+                        <option value="UNITED KINGDOM">UNITED KINGDOM</option>
+                        <option value="JAPAN">JAPAN</option>
+                        <option value="CANADA">CANADA</option>
+                        <option value="EUROPEAN UNION">EUROPEAN UNION</option>
+                    </select>
+                </div>
+            </nav>
+        </section>
+    );
+};
+
+export default Navbar;
